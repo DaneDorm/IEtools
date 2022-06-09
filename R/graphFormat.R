65dd61cc319fbd06c7b715191b7744afe3716df7
DaneDorm <102613820+DaneDorm@users.noreply.github.com>
2022-06-08 22:52:56 -0700

    basic vizual tools

---
+++ R/graphFormat.R
@@ -0,0 +1,35 @@
+#'This function formats visuals for using general rules
+#'@param  viz the visualization object, created with ggplot
+#'@param title a string for the title of the viz
+#'@param subtitle a string for the subtitle of the viz
+#'@param x a string for the title of the x axis of the viz
+#'@param y a string for the title of the y axis of the viz
+#'
+#'@return a visualization
+#'
+#'@import ggplot2
+#'@import extrafont
+#'
+#'@export
+ie_viz <- function(viz, title = NULL, subtitle = NULL, x = NULL, y = NULL) {
+
+  viz %>%
+    labs(x = x,
+         y = y,
+         subtitle = subtitle,
+         title = title) +
+    theme(plot.title.position = "plot", # Moves title/subtitle to top left
+          axis.text.y = element_text(angle = 0), # Rotate Tick Labels
+          axis.title.y = element_text(angle = 0, # Rotate Axis Label
+                                      vjust = .5),  # Move Label to mid
+          panel.grid.major = element_blank(), # Remove grid
+          panel.grid.minor = element_blank(), # Remove grid
+          plot.title = element_text(face = "bold",
+                                    family = "roboto"),
+          #family = "serif"), # Change text properties
+          plot.subtitle = element_text(face = "bold.italic",
+                                       color = "darkblue"), # Change text properties
+          panel.background = element_rect(fill = "gray95"), # Change background of just the graph
+          axis.line = element_line(color = "black"))
+
+}
